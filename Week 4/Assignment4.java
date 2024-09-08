5d1892410c956b8999e6626d3f9d4ff43a7a7de7
Subhayan Mukherjee <128389961+Templar121@users.noreply.github.com>
2024-09-08 08:54:51 +0530

    Add files via upload

---
+++ Week 4/Assignment4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+interface ExtraLarge
+{
+    String extra = "This is Extra-Large ";
+}
+class Large
+{
+    public void print()
+    {
+        System.out.println(This is Large);
+    }
+}
+class Medium extends Large
+{
+    public void print()
+    {
+        super.print();
+        System.out.println("This is Medium ");
+    }
+}
+class Small extends Medium
+{
+    public void print()
+    {
+        super.
+    }
+}
+public class Assignment4 {
+    
+}
