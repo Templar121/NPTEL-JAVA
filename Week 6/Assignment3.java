ed97d041cc78e529a13ebe1a0ca24f14b526ce9c
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-01-28 07:14:08 +0530

    update assignment 3

---
+++ Week 6/Assignment3.java
@@ -0,0 +1,13 @@
+class Assignment3 extends Thread{ 
+	public void run(){
+		System.out.print("Thread is Running.");
+	}
+public static void main(String args[]){  
+
+		// Creating object of thread class
+		Assignment3 thread=new W06_P3();  
+
+                // Start the thread
+		thread.start();
+	}  
+}
